36f97c477ef3144283b998a63a5cf980301af2eb
zxxzxx1993 <1803866319@qq.com>
2016-11-01 15:30:16 +0800

    添加butternife和欢迎页面

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -2,14 +2,29 @@ package cn.ucai.superwechat.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.utils.MFGT;
 
 public class GuideActivity extends AppCompatActivity {
-
+   GuideActivity context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
         setContentView(R.layout.activity_guide);
+    }
+    @OnClick
+    public void onclick(View view){
+        switch (view.getId()){
+            case  R.id.img_login:
+                MFGT.gotoLogin(context);
+                break;
+            case R.id.img_register:
+                MFGT.gotoRegister(context);
+                break;
+        }
     }
 }
---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
@@ -0,0 +1,32 @@
+package cn.ucai.superwechat.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.ui.LoginActivity;
+import cn.ucai.superwechat.ui.MainActivity;
+import cn.ucai.superwechat.ui.RegisterActivity;
+
+
+public class MFGT {
+    public static void finish(Activity activity){
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+    public static void gotoMainActivity(Activity context){
+        startActivity(context, MainActivity.class);
+    }
+    public static void startActivity(Activity context,Class<?> cls){
+        Intent intent = new Intent();
+        intent.setClass(context,cls);
+        context.startActivity(intent);
+        context.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+    }
+public static void gotoLogin(Activity context){
+    startActivity(context, LoginActivity.class);
+}
+    public static void gotoRegister(Activity context){
+        startActivity(context, RegisterActivity.class);
+    }
+}
